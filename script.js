d87a985410e778d11f45f5ca06246f4a6181c6ac
Almyra Valdez <almyrajannelle.valdez@student.csulb.edu>
2018-01-27 14:40:53 -0800

    Fix unexpected behavior in range

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ function getRndInteger(min, max) {
     var max = Math.floor(max);
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+function getValues(){
+  var maxNum = $('#max').val();
+  var minNum = $('#min').val();
+  var values=[maxNum,minNum];
+  return values;
+}
 $(document).ready(function(){
-  var randNum;
   $('button[name="roll"]').click(function(){
-    var maxNum = $('#max').val();
-    var minNum = $('#min').val();
-    if(maxNum<0 || minNum<0){
+    var range = getValues();
+    console.log("Max: " + range[0] + "\nMin:" + range[1]);
+    if(range[0]<0 || range[1]<0){
       $('#number').text('No negative numbers please!');
-    }
-    if(minNum>maxNum){
+      return;
+    } 
+    if(range[1]>=range[0] && !(range[1]<10)){
       $('#number').text('Max must be greater than min!');
-    }
-    $('#number').text('You rolled a '+getRndInteger(minNum, maxNum)+'.');
+      return;
+    } 
+     $('#number').text('You rolled a '+getRndInteger(range[1], range[0])+'.');
   });
 });
 
