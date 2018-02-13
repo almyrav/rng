6826b4f17d7abfefc67c5cf595189b3c649ee491
Almyra Valdez <almyrajannelle.valdez@student.csulb.edu>
2018-02-13 15:31:16 -0800

    Fix comparison between max and min input

--- script.js
+++ script.js
@@ -3,25 +3,20 @@ function getRndInteger(min, max) {
     var max = Math.floor(max);
     return Math.floor(Math.random()*(max-min+1)+min);
 }
-function getValues(){
-  var maxNum = $('#max').val();
-  var minNum = $('#min').val();
-  var values=[maxNum,minNum];
-  return values;
-}
+
 $(document).ready(function(){
   $('button[name="roll"]').click(function(){
-    var range = getValues();
-    console.log("Max: " + range[0] + "\nMin:" + range[1]);
-    if(range[0]<0 || range[1]<0){
+    var maxNum = $('#max').val();
+    var minNum = $('#min').val();
+    if(maxNum<0 || minNum<0){
       $('#number').text('No negative numbers please!');
       return;
     } 
-    if(range[1]>=range[0] && !(range[1]<10)){
+    if(Math.abs(minNum)>=Math.abs(maxNum)){
       $('#number').text('Max must be greater than min!');
       return;
     } 
-     $('#number').text('You rolled a '+getRndInteger(range[1], range[0])+'.');
+     $('#number').text('You rolled a '+getRndInteger(minNum, maxNum)+'.');
   });
 });
 
